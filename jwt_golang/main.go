b0c25ee15c086f0ecc5ca8e732a12118d8bc05cc
oppp0999 <oppp0999@naver.com>
2022-12-04 20:28:49 +0900

    20221204_2

---
+++ jwt_golang/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/twinj/uuid"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"jwt_golnag/common"
+)
+
+// 3. 토큰 정의, 만료 기간 및 UUID 구조체
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUuid   string
+	RefreshUuid  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+// router 객체
+var (
+	router = gin.Default()
+)
+
+// 사용자 객체 생성
+type User struct {
+	ID       uint64 `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var test_user = User{
+	ID:       1,
+	Username: "heejae",
+	Password: "1234",
+}
+
+// 로그인 요청 메소드
+func Login(c *gin.Context) {
+	var u User
+	//u의 값을 바이너리 값으로 변환 후에 err에 저장 후에 비어있는지 체크
+	if err := c.ShouldBindJSON(&u); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+		return
+	}
+	//compare the user from the request, with the one we defined: 확인 -> 요청제거
+	if test_user.Username != u.Username || test_user.Password != u.Password {
+		c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+		return
+	}
+	token, err := CreateToken(test_user.ID)
+	if err != nil {
+		c.JSON(http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	c.JSON(http.StatusOK, token)
+}
+
+// 4. CreateToken method
+func CreateToken(userID uint64) (*TokenDetails, error) {
+	td := &TokenDetails{}
+	//엑세스 토큰 15분
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUuid = uuid.NewV4().String()
+	//리프래시 토큰 7일
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUuid = uuid.NewV4().String()
+
+	var err error
+	//Create Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUuid
+	atClaims["user_id"] = userID
+	atClaims["exp"] = td.AtExpires
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+
+	//Create Refresh Token
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf") //this should be in an env file
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = userID
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+	return td, nil
+}
+
+// 5. JWT 매타데이터의 저장
+func CreateAuth(userid uint64, td *TokenDetails) error {
+	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC
+	rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
+
+	errAccess := common.Client.Set(td.AccessUuid, strconv.Itoa(int(userid)), at.Sub(now)).Err()
+	if errAccess != nil {
+		return errAccess
+	}
+	errRefresh := common.Client.Set(td.RefreshUuid, strconv.Itoa(int(userid)), rt.Sub(now)).Err()
+	if errRefresh != nil {
+		return errRefresh
+	}
+	return nil
+}
+
+// main
+func main() {
+	router.POST("/login", Login)
+	log.Fatal(router.Run(":8080"))
+}
